5f04d52cdbfdb785c11e37f8467b5eb402f009ef
Rin <me@lo-li.net>
2020-07-21 23:13:23 +0800

    add frontend

---
+++ build/precache-manifest.16c78ee909355a6b6a8b001c3274ccd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a50fc2909bd82e23f9cb94974925ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20897ecdde6bcc6d886e",
+    "url": "/static/css/main.837571a9.chunk.css"
+  },
+  {
+    "revision": "04aa0c57b87cf34d196a",
+    "url": "/static/js/2.efba5cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efba5cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20897ecdde6bcc6d886e",
+    "url": "/static/js/main.7639a53a.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
